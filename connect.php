d38c1952f7a1a1c1e4df6fb172d8fd50866c5728
tirlipinpon <tirlipinpon@hotmail.com>
2016-07-07 18:26:06 +0200

    get data and return json parse

---
+++ connect.php
@@ -0,0 +1,17 @@
+<?php
+ $DBhost = "localhost";
+ $DBuser = "root";
+ $DBpass = "";
+ $DBname = "boysbandmuz";
+
+ try{
+
+  $dbh = new PDO("mysql:host=$DBhost;dbname=$DBname",$DBuser,$DBpass);
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+ }catch(PDOException $ex){
+
+  die( $ex->getMessage());
+ }
+
+?>
---
+++ datas.php
@@ -0,0 +1,27 @@
+<?php
+  include_once "connect.php";
+  function readDataForwards($dbh) {
+    $sql = 'SELECT * FROM musique';
+    try {
+      $stmt = $dbh->prepare($sql, array(PDO::ATTR_CURSOR, PDO::CURSOR_SCROLL));
+      $stmt->execute();
+      $datas = $stmt->fetch(PDO::FETCH_ASSOC);
+      //var_dump($datas);
+        echo json_encode($datas);
+      $stmt = null;
+    }
+    catch (PDOException $e) {
+      print $e->getMessage();
+    }
+  }
+  readDataForwards($dbh);
+
+/*
+  echo"<p>";
+  echo ("============= mock ==========================");
+  $arr = array('id' => 1, 'titre' => 'aaa', 'url' =>'bbb');
+  var_dump($arr);
+  echo json_encode($arr);
+  echo"</p>";
+  */
+?>
